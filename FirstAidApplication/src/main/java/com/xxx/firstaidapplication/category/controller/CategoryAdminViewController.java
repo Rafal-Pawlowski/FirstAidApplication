14d47507ff2afee870d253a24b3c04e912abfe3b
Rafal <rafalpawlowski.poczta@gmail.com>
2024-02-27 22:08:16 +0100

    edit method in CategoryAdminViewController @Valid Added global message.class Lombok Dependency added

--- FirstAidApplication/src/main/java/com/xxx/firstaidapplication/category/controller/CategoryAdminViewController.java
+++ FirstAidApplication/src/main/java/com/xxx/firstaidapplication/category/controller/CategoryAdminViewController.java
@@ -2,9 +2,13 @@ package com.xxx.firstaidapplication.category.controller;
 
 import com.xxx.firstaidapplication.category.domain.model.Category;
 import com.xxx.firstaidapplication.category.service.CategoryService;
+import com.xxx.firstaidapplication.common.dto.Message;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.UUID;
 
@@ -34,8 +38,27 @@ public class CategoryAdminViewController {
     }
 
     @PostMapping("{id}")
-    public String edit(@ModelAttribute("category") Category category,@PathVariable UUID id){
-        categoryService.updateCategory(id, category);
+    public String edit(
+            @PathVariable UUID id,
+            @Valid @ModelAttribute("category") Category category,
+            BindingResult bindingResult,
+            RedirectAttributes ra,
+            Model model
+    ){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("category", category);
+            model.addAttribute("message", Message.error("Błąd zapisu"));
+        return "admin/category/edit";
+        }
+
+        try{
+            categoryService.updateCategory(id, category);
+            ra.addFlashAttribute("message", Message.info("Kategoria zapisana pomyślnie"));
+        }catch (Exception e){
+            model.addAttribute("category", category);
+            model.addAttribute("message", Message.error("Nieznany błąd zapisu"));
+            return "admin/category/edit";
+        }
         return "redirect:/admin/categories";
     }
 
